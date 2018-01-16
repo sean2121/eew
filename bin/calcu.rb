38fcda81c97f271c128e1cc2e8e22f9a87759de1
shou <shou@shounoMacBook-puro.local>
2018-01-16 11:16:03 +0900

    first commit

---
+++ bin/calcu.rb
@@ -0,0 +1,21 @@
+class calcu
+
+
+  
+  Struct.new()
+
+  def initialize 
+  end
+
+  def max_acceleration
+  end
+
+  def max_speed
+  end
+
+  def intensity
+  end
+
+  def reach_time
+  end 
+end
