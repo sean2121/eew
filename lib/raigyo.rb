7edc8177c2f66fc27d566f59110a2c1caef6aebc
sean2121 <shouto04@gmail.com>
2018-02-28 18:23:01 +0900

    twitter bot add

--- lib/raigyo.rb
+++ lib/raigyo.rb
@@ -3,8 +3,27 @@ require 'pry'
 require './raigyo/user.rb'
 require './raigyo/mailman.rb'
 require 'dotenv/load'
+require "twitter"
 
 
+Dotenv.load
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['ConsumerKey']
+  config.consumer_secret     = ENV['ConsumerSecret']
+end
+
+Thread.start{
+  loop do 
+    client.user_timeline('eewbot', count: 1 ).each do |tweet|
+     p tweet.text
+    end
+  end 
+}
+
+  loop do 
+    puts "hello"
+    sleep 0.5
+  end
 
 #発生地
 latitude_s = 38.4385
@@ -16,8 +35,6 @@ longitude_e = 142.864177
 
 depth = 24
 mjma = 8.4
-
-p ENV['S3_BUCKET']
 
 Mailman.new(latitude_s, longitude_s, mjma, depth, User.new(latitude_e, longitude_e, 1))
 
