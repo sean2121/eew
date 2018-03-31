12630fc1499ddd767d94141a84e92f5191f91777
sean2121 <shouto04@gmail.com>
2018-03-31 14:55:07 +0900

    add readme:

--- lib/eew/main.rb
+++ lib/eew/main.rb
@@ -17,8 +17,6 @@ client = Twitter::REST::Client.new do |config|
   config.consumer_secret     = ENV['ConsumerSecret']
 end
 
-
-
 loop do
   #begin
   client.user_timeline('eewbot', count: 1 ).each do |tweet|
@@ -26,10 +24,8 @@ loop do
       threads_mutex = Mutex.new
       threads_mutex.synchronize do
       eew = Parser.new(tweet.full_text).parse
+      p eew
       latitude_s, longitude_s, mjma, depth, announce_time, earthquake_time = eew[:latitude_s], eew[:longitude_s], eew[:mjma], eew[:depth], eew[:announce_time], eew[:earthquake_time]
-      eew.each do |elem|
-        p elem
-      end
       earthquake = Mailman.new(latitude_s.to_f, longitude_s.to_f, mjma.to_f, depth.to_f, announce_time, earthquake_time)
       earthquake.add_user(User.new(latitude_e, longitude_e, 1))
       earthquake.notify_to_user
